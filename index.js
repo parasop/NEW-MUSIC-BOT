2242255d634a7ac1d2a08519ea5651ce5b72df5f
parasop <makvanaparas564@gmail.com>
2022-04-08 14:12:12 +0000

    v14 #1 video

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const discord = require("discord.js"); 
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+require("dotenv").config();
 const client = new discord.Client({
-  disableEveryone: true 
+  disableEveryone: true,
+  intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildVoiceStates,GatewayIntentBits.GuildMembers,GatewayIntentBits.GuildMessages],
 })
-const {Database} = require("quickmongo")
+//const {Database} = require("quickmongo")
 
 
 
 client.config = require("./config.json")
-client.db = new Database(client.config.DB)
+//client.db = new Database(client.config.DB)
 client.formatDuration  = require("./paras/formatDuration")
 require("./paras/Player")
 client.commands = new discord.Collection();
@@ -18,11 +21,7 @@ client.aliases = new discord.Collection();
 });
 
 
-client.db.on("ready",() => {
-  
-  console.log("[DB] QUICK CONNECTED")
-})
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 		const dbOptions = {
 			useNewUrlParser: true,
@@ -43,6 +42,6 @@ const mongoose = require('mongoose');
 		});
 		mongoose.connection.on('disconnected', () => {
 			console.log('Mongoose disconnected');
-		});
+		});*/
 
 client.login(process.env.TOKEN);
